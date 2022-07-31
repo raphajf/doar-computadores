6439a2a44d0d1d9706830b664ce878c0dd823192
raphajf <101059493+raphajf@users.noreply.github.com>
2022-07-31 11:42:29 -0300

    delete src/services

--- src/services/form.service.js
+++
@@ -1,9 +0,0 @@
-function receiveForm (form) {
-    if (parseInt(form.deviceCount) !== form.devices.length){
-        throw new Error({error:true, errorMessage: `A quantidade de equipamentos (${form.deviceCount}) não está de acordo com as informações de equipamentos enviados (${form.devices.length})`});
-    } else {
-        return { success:true };
-    }
-}
-
-module.exports = receiveForm;
